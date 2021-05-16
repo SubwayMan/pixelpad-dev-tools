d22b80f560a3bee2b829cd820ee2b5264aa5864a
SubwayMan <tyche4490@gmail.com>
2021-05-16 00:19:46 -0700

    Added catch for bad auth, preparing to switch to proper login protocol

--- tester.py
+++ tester.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import auth
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+try:
+	import auth
+
+except ModuleNotFoundError:
+
+	print("Login details missing. Setting up auth...")
+	usern = input("Enter name: ")
+	userp = input("Enter password: ")
+
+	au = open("auth.py", "w")
+	out = "name = \"" + usern + "\"\npwd = \"" + userp + "\""
+	au.write(out)
+	au.close()
+	import auth
 
 PATH = "C:\Program Files (x86)\chromedriver.exe" 
 driver = webdriver.Chrome(PATH)
