2060a9171f983abec583bf66835d99fdfc263b95
SubwayMan <tyche4490@gmail.com>
2021-05-16 17:40:38 -0700

    Finished main feature, logging is working, file successfully imports.

--- tester.py
+++ tester.py
@@ -1,9 +1,11 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
 
 try:
     import auth
@@ -40,6 +42,7 @@ driver.get("https://pixelpad.io")
 try:
     elem = get_element(By.ID, "pixelpad_login")
     elem.click()
+    
     emailfield = get_element(By.ID, "login_username")
     passfield = get_element(By.ID, "login_password")
     emailfield.send_keys(auth.name)
@@ -48,7 +51,6 @@ try:
 
     myappbut = get_element(By.LINK_TEXT, "MY PAD")
     myappbut.click()
-
     makeappbut = get_element(By.ID, "create_a_new_app_button")
     makeappbut.click()
     namefield = get_element(By.ID, "appcreate_name")
@@ -60,7 +62,29 @@ try:
     close_docs.click()
     driver.find_element_by_xpath("//div[@id='pixelpad_nav_topbar']/div[1]/div[1]").click()
     driver.find_element_by_link_text("IMPORT").click() 
-    driver.find_element_by_id("importProject").click()  
+
+    fileinp = driver.find_element_by_id("importProject")
+    fileinp.send_keys(os.path.join(os.getcwd(), "inp.pp2d"))
+    driver.implicitly_wait(2)
+    driver.find_element_by_id("importProjectButton").click()
+
+    driver.switch_to.alert.accept()
+    try:
+        driver.implicitly_wait(2)
+        driver.find_element_by_xpath("//div[@id='importModal']/div[1]/div[1]/div[2]/button[1]").click()
+
+    except selenium.common.exceptions.NoSuchElementException:
+        pass
+
+    print("Running pixelpad file...")
+    driver.find_element_by_id("pp-start").click()
+    driver.implicitly_wait(5)
+    
+    outfile = open("out.txt", "w")
+    cont = driver.find_element_by_id("output").text
+    outfile.write(cont)
+    outfile.close()
+    print("PixelPad file successfully ran. Output logged.")
 
 except:
     driver.close()
